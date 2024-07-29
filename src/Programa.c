614b7984c3edf934d9a9729abbcaa2ad8ce58547
convidado <convidado@lab-02-07-20.dcomp>
2024-07-29 15:01:20 -0300

    Inputs

--- src/Programa.c
+++ src/Programa.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
+#include <string.h>
+
+#define N 1024
+#define M 10
+
+void tirar_barra_N(char *string);
 
 int main(){
-    printf("Olá mundo!\n");
+
+    char string[N], sub_string[M];
+    int K, inicio_intervalo, fim_intervalo;
+
+    fgets(string, sizeof(string), stdin);
+    fgets(sub_string, sizeof(sub_string), stdin);
+
+    tirar_barra_N(string);
+    tirar_barra_N(sub_string);
+
+    scanf("%i", &K);
+
+    for(int i = 0; i < K; ++i){
+        scanf("%i %i", &inicio_intervalo, &fim_intervalo);
+    }
+
+    printf("%s\n", string);
+    printf("%s\n", sub_string);
+
+
 
     return 0;
+}
+
+void tirar_barra_N(char *string){
+    int tamanho = strlen(string);
+    string[tamanho - 1] = '\0';
 }
